3828c06cb2e7d2e5aa0224e87bd44c50551379d4
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-06-08 16:29:51 -0700

    Add basic Doxygen comments and modern main

--- src/cmd/chown.c
+++ src/cmd/chown.c
@@ -4,64 +4,72 @@
  *   All Rights Reserved. 					      *
  *   Reference "/usr/src/COPYRIGHT" for applicable restrictions.      *
  **********************************************************************/
+/**
+ * @file chown.c
+ * @brief Change file owner.
+ */
 
 /*
  * chown uid file ...
  */
 
 static char Sccsid[] = "@(#)chown.c 3.0 4/21/86";
-#include <stdio.h>
 #include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <pwd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-struct	passwd	*pwd,*getpwnam();
-struct	stat	stbuf;
-int	uid;
-int	status;
+struct passwd *pwd, *getpwnam();
+struct stat stbuf;
+int uid;
+int status;
 
-main(argc, argv)
-char *argv[];
-{
-	register c;
+/**
+ * @brief Program entry point.
+ *
+ * @param argc Argument count.
+ * @param argv Argument vector.
+ * @return EXIT_SUCCESS on success.
+ */
+int main(int argc, char **argv) {
+  register c;
 
-	if(argc < 3) {
-		printf("usage: chown uid file ...\n");
-		exit(4);
-	}
-	if(isnumber(argv[1])) {
-		uid = atoi(argv[1]);
-		goto cho;
-	}
-	if((pwd=getpwnam(argv[1])) == NULL) {
-		printf("unknown user id: %s\n",argv[1]);
-		exit(4);
-	}
-	uid = pwd->pw_uid;
+  if (argc < 3) {
+    printf("usage: chown uid file ...\n");
+    exit(4);
+  }
+  if (isnumber(argv[1])) {
+    uid = atoi(argv[1]);
+    goto cho;
+  }
+  if ((pwd = getpwnam(argv[1])) == NULL) {
+    printf("unknown user id: %s\n", argv[1]);
+    exit(4);
+  }
+  uid = pwd->pw_uid;
 
 cho:
-	for(c=2; c<argc; c++) {
-		stat(argv[c], &stbuf);
-		if(chown(argv[c], uid, stbuf.st_gid) < 0) {
-			perror(argv[c]);	
+  for (c = 2; c < argc; c++) {
+    stat(argv[c], &stbuf);
+    if (chown(argv[c], uid, stbuf.st_gid) < 0) {
+      perror(argv[c]);
 
-			/* SYSTEM V compatibility */
-			/* printf("%s: permission denied, must be super-user\n", 
-				argv[c]); */
-			status = 1;
-		}
-	}
-	exit(status);
+      /* SYSTEM V compatibility */
+      /* printf("%s: permission denied, must be super-user\n",
+              argv[c]); */
+      status = 1;
+    }
+  }
+  exit(status);
 }
 
-isnumber(s)
-char *s;
+isnumber(s) char *s;
 {
-	register c;
+  register c;
 
-	while(c = *s++)
-		if(!isdigit(c))
-			return(0);
-	return(1);
+  while (c = *s++)
+    if (!isdigit(c))
+      return (0);
+  return (1);
 }
--- src/cmd/clri.c
+++ src/cmd/clri.c
@@ -4,102 +4,109 @@
  *   All Rights Reserved. 					      *
  *   Reference "/usr/src/COPYRIGHT" for applicable restrictions.      *
  **********************************************************************/
+/**
+ * @file clri.c
+ * @brief Clear inode.
+ */
 
 /*
  * clri filsys inumber ...
  */
 
 static char Sccsid[] = "@(#)clri.c 3.0 4/21/86";
-#ifdef	UCB_NKB
+#ifdef UCB_NKB
 #include <sys/param.h>
 #else
 #include <sys/types.h>
-#endif	UCB_NKB
+#endif UCB_NKB
 #include <sys/ino.h>
-#define ISIZE	(sizeof(struct dinode))
-#ifndef	UCB_NKB
-#define	BSIZE	512
-#endif	UCB_NKB
-#define	NI	(BSIZE/ISIZE)
-struct	ino
-{
-	char	junk[ISIZE];
+#define ISIZE (sizeof(struct dinode))
+#ifndef UCB_NKB
+#define BSIZE 512
+#endif UCB_NKB
+#define NI (BSIZE / ISIZE)
+struct ino {
+  char junk[ISIZE];
 };
-struct	ino	buf[NI];
-int	status;
+struct ino buf[NI];
+int status;
 
-main(argc, argv)
-char *argv[];
-{
-	register i, f;
-	unsigned n;
-	int j, k;
-	long off;
+/**
+ * @brief Program entry point.
+ *
+ * @param argc Argument count.
+ * @param argv Argument vector.
+ * @return EXIT_SUCCESS on success.
+ */
+int main(int argc, char **argv) {
+  register i, f;
+  unsigned n;
+  int j, k;
+  long off;
 
-	if(argc < 3) {
-		printf("usage: clri filsys inumber ...\n");
-		exit(4);
-	}
-	f = open(argv[1], 2);
-	if(f < 0) {
-		printf("cannot open %s\n", argv[1]);
-		exit(4);
-	}
-	for(i=2; i<argc; i++) {
-		if(!isnumber(argv[i])) {
-			printf("%s: is not a number\n", argv[i]);
-			status = 1;
-			continue;
-		}
-		n = atoi(argv[i]);
-		if(n == 0) {
-			printf("%s: is zero\n", argv[i]);
-			status = 1;
-			continue;
-		}
-#ifdef	UCB_NKB
-		off = itod(n) * BSIZE;
+  if (argc < 3) {
+    printf("usage: clri filsys inumber ...\n");
+    exit(4);
+  }
+  f = open(argv[1], 2);
+  if (f < 0) {
+    printf("cannot open %s\n", argv[1]);
+    exit(4);
+  }
+  for (i = 2; i < argc; i++) {
+    if (!isnumber(argv[i])) {
+      printf("%s: is not a number\n", argv[i]);
+      status = 1;
+      continue;
+    }
+    n = atoi(argv[i]);
+    if (n == 0) {
+      printf("%s: is zero\n", argv[i]);
+      status = 1;
+      continue;
+    }
+#ifdef UCB_NKB
+    off = itod(n) * BSIZE;
 #else
-		off = ((n-1)/NI + 2) * (long)512;
-#endif	UCB_NKB
-		lseek(f, off, 0);
-		if(read(f, (char *)buf, BSIZE) != BSIZE) {
-			printf("%s: read error\n", argv[i]);
-			status = 1;
-		}
-	}
-	if(status)
-		exit(status);
-	for(i=2; i<argc; i++) {
-		n = atoi(argv[i]);
-		printf("clearing %u\n", n);
-#ifdef	UCB_NKB
-		off = itod(n) * BSIZE;
+    off = ((n - 1) / NI + 2) * (long)512;
+#endif UCB_NKB
+    lseek(f, off, 0);
+    if (read(f, (char *)buf, BSIZE) != BSIZE) {
+      printf("%s: read error\n", argv[i]);
+      status = 1;
+    }
+  }
+  if (status)
+    exit(status);
+  for (i = 2; i < argc; i++) {
+    n = atoi(argv[i]);
+    printf("clearing %u\n", n);
+#ifdef UCB_NKB
+    off = itod(n) * BSIZE;
 #else
-		off = ((n-1)/NI + 2) * (long)512;
-#endif	UCB_NKB
-		lseek(f, off, 0);
-		read(f, (char *)buf, BSIZE);
-#ifdef	UCB_NKB
-		j = itoo(n);
+    off = ((n - 1) / NI + 2) * (long)512;
+#endif UCB_NKB
+    lseek(f, off, 0);
+    read(f, (char *)buf, BSIZE);
+#ifdef UCB_NKB
+    j = itoo(n);
 #else
-		j = (n-1)%NI;
-#endif	UCB_NKB
-		for(k=0; k<ISIZE; k++)
-			buf[j].junk[k] = 0;
-		lseek(f, off, 0);
-		write(f, (char *)buf, BSIZE);
-	}
-	exit(status);
+    j = (n - 1) % NI;
+#endif UCB_NKB
+    for (k = 0; k < ISIZE; k++)
+      buf[j].junk[k] = 0;
+    lseek(f, off, 0);
+    write(f, (char *)buf, BSIZE);
+  }
+  exit(status);
 }
 
-isnumber(s)
-char *s;
+isnumber(s) char *s;
 {
-	register c;
+  register c;
 
-	while(c = *s++)
-		if(c < '0' || c > '9')
-			return(0);
-	return(1);
+  while (c = *s++)
+    if (c < '0' || c > '9')
+      return (0);
+  return (1);
 }
